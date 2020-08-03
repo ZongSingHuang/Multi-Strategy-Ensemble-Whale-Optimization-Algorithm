6ed5878cefc407f79067d1320867b87093723b52
ZongSingHuang <58209813+ZongSingHuang@users.noreply.github.com>
2020-08-03 17:47:30 +0800

    2020.08.03

---
+++ MSWOA.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 16 21:59:58 2020
+
+@author: ZongSing_NB
+
+Main reference:http://www.alimirjalili.com/WOA.html
+"""
+
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+class MSWOA():
+    def __init__(self, fit_func, num_dim=30, num_particle=20, max_iter=500,
+                 b=1, x_max=1, x_min=0, a_max=2, a_min=0, l_max=1, l_min=-1, a2_max=-1, a2_min=-2):
+        self.fit_func = fit_func        
+        self.num_dim = num_dim
+        self.num_particle = num_particle
+        self.max_iter = max_iter     
+        self.x_max = x_max
+        self.x_min = x_min
+        self.a_max = a_max
+        self.a_min = a_min
+        self.a2_max = a2_max
+        self.a2_min = a2_min
+        self.l_max = l_max
+        self.l_min = l_min
+        self.b = b
+
+        self._iter = 1
+        self.gBest_X = None
+        self.gBest_score = np.inf
+        self.gBest_curve = np.zeros(self.max_iter)
+        
+        self.X = np.random.uniform(low=-1.0, high=1.0, size=[self.num_particle, self.num_dim])
+        self.X = 1 - 2*( np.cos( 4*np.arccos(self.X) ) )**2
+        self.X = (self.X+1) / 2
+        self.X = self.X*(self.x_max-self.x_min) + self.x_min
+        
+        score = self.fit_func(self.X)
+        self.gBest_score = score.min().copy()
+        self.gBest_X = self.X[score.argmin()].copy()
+        self.gBest_curve[0] = self.gBest_score.copy()
+        
+    def opt(self):
+        while(self._iter<self.max_iter):
+            a = self.a_max - (self.a_max-self.a_min)*(self._iter/self.max_iter)
+            # a = 2*np.cos(self._iter/self.max_iter)
+            
+            for i in range(self.num_particle):
+                p = np.random.uniform()
+                R1 = np.random.uniform()
+                R2 = np.random.uniform()
+                R3 = np.random.uniform()
+                R4 = np.random.uniform()
+                R5 = np.random.uniform()
+                R6 = np.random.uniform()
+                A = 2*a*R1 - a
+                C = 2*R2
+                l = np.random.uniform()*(self.l_max-self.l_min) + self.l_min
+
+                if p<0.5:
+                    if np.abs(A)<1:
+                        D = np.abs(C*self.gBest_X - self.X[i, :])
+                        self.X[i, :] = self.gBest_X - A*D
+                    else:
+                        self.X[i, :] = self.X[i, :]*R3 + 2*R4*np.abs(self.gBest_X - self.X[i, :])
+                else:                    
+                    D = np.abs(self.gBest_X - self.X[i, :])
+                    if np.abs(A)<1:
+                        self.X[i, :] = self.gBest_X + D*np.exp(self.b*l)*np.cos(2*np.pi*l)*self.levy(size=30)
+                    else:
+                        self.X[i, :] = self.X[i, :] + D*np.exp(self.b*l)*np.cos(2*np.pi*l)*self.levy(size=30)
+                
+                
+                bound_max = np.dot(np.ones(self.num_particle)[:, np.newaxis], self.x_max[np.newaxis, :])
+                bound_min = np.dot(np.ones(self.num_particle)[:, np.newaxis], self.x_min[np.newaxis, :])
+                idx_too_high = bound_max < self.X
+                idx_too_low = bound_min > self.X
+                bound_max_map = bound_max[idx_too_high] + \
+                                R5*bound_max[idx_too_high]*(bound_max[idx_too_high]-self.X[idx_too_high])/self.X[idx_too_high]
+                bound_min_map = bound_min[idx_too_low] + \
+                                R6*np.abs(bound_min[idx_too_low]*(bound_min[idx_too_low]-self.X[idx_too_low])/self.X[idx_too_low])
+                if np.any(bound_max_map==np.inf) or np.any(bound_min_map==np.inf):                   
+                    print(123)                
+                self.X[idx_too_high] = bound_max_map.copy()
+                self.X[idx_too_low] = bound_min_map.copy()
+                score = self.fit_func(self.X)
+                if np.min(score) < self.gBest_score:
+                    self.gBest_X = self.X[score.argmin()].copy()
+                    self.gBest_score = score.min().copy()
+                
+            self.gBest_curve[self._iter] = self.gBest_score.copy()    
+            self._iter = self._iter + 1
+        
+    def plot_curve(self):
+        plt.figure()
+        plt.title('loss curve ['+str(round(self.gBest_curve[-1], 3))+']')
+        plt.plot(self.gBest_curve, label='loss')
+        plt.grid()
+        plt.legend()
+        plt.show()  
+        
+    def levy(self, size=1):
+        beta = 1.5
+        
+        sigma_u_up = math.gamma(1+beta) * np.sin(np.pi*beta/2)
+        sigma_u_down = math.gamma((1+beta)/2) * beta * 2**((beta-1)/2)
+        sigma_u = (sigma_u_up / sigma_u_down)**(1/beta)
+        
+        u = np.random.normal(0, sigma_u**2, size=size)
+        v = np.random.normal(0, 1, size=size)
+        s = u / ( np.abs(v)**(1/beta) )
+        
+        return s
+            
---
+++ main.py
@@ -0,0 +1,260 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 16 21:43:03 2020
+
+@author: ZongSing_NB
+"""
+
+from MSWOA import MSWOA
+import numpy as np
+import time
+import pandas as pd
+
+def Sphere(x):
+    if x.ndim==1:
+        x = x.reshape(1, -1)
+    return np.sum(x**2, axis=1)
+
+def Schwefel_P222(x):
+    if x.ndim==1:
+        x = x.reshape(1, -1)
+    return np.sum(np.abs(x), axis=1) + np.prod(np.abs(x), axis=1)
+
+def Quadric(x):
+    if x.ndim==1:
+        x = x.reshape(1, -1)
+    
+    outer = 0
+    for i in range(x.shape[1]):
+        inner = np.sum(x[:, :i+1], axis=1)**2
+        outer = outer + inner
+    
+    return outer
+
+def Rosenbrock(x):
+    if x.ndim==1:
+        x = x.reshape(1, -1) 
+    
+    left = x[:, :-1].copy()
+    right = x[:, 1:].copy()
+    
+    return np.sum(100*(right - left**2)**2 + (left-1)**2, axis=1)
+
+def Step(x):
+    if x.ndim==1:
+        x = x.reshape(1, -1)
+    return np.sum(np.round((x+0.5), 0)**2, axis=1)
+
+def Quadric_Noise(x):
+    if x.ndim==1:
+        x = x.reshape(1, -1)
+        
+    matrix = np.arange(x.shape[1])+1
+     
+    return np.sum((x**4)*matrix, axis=1)+np.random.rand(x.shape[0])
+
+def Schwefel(x):
+    if x.ndim==1:
+        x = x.reshape(1, -1)        
+     
+    return -1*np.sum(x*np.sin(np.abs(x)**.5), axis=1)
+
+def Rastrigin(x):
+    if x.ndim==1:
+        x = x.reshape(1, -1) 
+    
+    return np.sum(x**2 - 10*np.cos(2*np.pi*x) + 10, axis=1)
+
+def Noncontinuous_Rastrigin(x):
+    if x.ndim==1:
+        x = x.reshape(1, -1)
+    
+    outlier = np.abs(x)>=0.5
+    x[outlier] = np.round(2*x[outlier])/2
+    
+    return np.sum(x**2 - 10*np.cos(2*np.pi*x) + 10, axis=1)
+
+def Ackley(x):
+    if x.ndim==1:
+        x = x.reshape(1, -1)
+    
+    left = 20*np.exp(-0.2*(np.sum(x**2, axis=1)/x.shape[1])**.5)
+    right = np.exp(np.sum(np.cos(2*np.pi*x), axis=1)/x.shape[1])
+    
+    return -left - right + 20 + np.e
+
+def Griewank(x):
+    if x.ndim==1:
+        x = x.reshape(1, -1)
+    left = np.sum(x**2, axis=1)/4000
+    right = np.prod( np.cos(x/((np.arange(x.shape[1])+1)**.5)), axis=1)
+    return left - right + 1
+
+
+d = 30
+g = 500
+p = 20
+times = 5
+table = np.zeros((4, 11))
+for i in range(times):
+    worst = -np.inf
+    best = np.inf
+    x_max = 100*np.ones(d)
+    x_min = -100*np.ones(d)
+    optimizer = WOA(fit_func=Sphere, 
+                    num_dim=d, num_particle=p, max_iter=g, x_max=x_max, x_min=x_min)
+    start = time.time()
+    optimizer.opt()
+    end = time.time()
+    if optimizer.gBest_score>worst: worst = optimizer.gBest_score
+    if optimizer.gBest_score<best: best = optimizer.gBest_score
+    table[0, 0] += optimizer.gBest_score
+    table[1, 0] += end - start
+    table[2, 0] = worst
+    table[3, 0] = best    
+    
+    x_max = 10*np.ones(d)
+    x_min = -10*np.ones(d)
+    optimizer = WOA(fit_func=Schwefel_P222,
+                    num_dim=d, num_particle=p, max_iter=g, x_max=x_max, x_min=x_min)
+    start = time.time()
+    optimizer.opt()
+    end = time.time()
+    if optimizer.gBest_score>worst: worst = optimizer.gBest_score
+    if optimizer.gBest_score<best: best = optimizer.gBest_score
+    table[0, 1] += optimizer.gBest_score
+    table[1, 1] += end - start
+    table[2, 1] = worst
+    table[3, 1] = best    
+    
+    x_max = 100*np.ones(d)
+    x_min = -100*np.ones(d)
+    optimizer = WOA(fit_func=Quadric,
+                    num_dim=d, num_particle=p, max_iter=g, x_max=x_max, x_min=x_min)
+    start = time.time()
+    optimizer.opt()
+    end = time.time()
+    if optimizer.gBest_score>worst: worst = optimizer.gBest_score
+    if optimizer.gBest_score<best: best = optimizer.gBest_score    
+    table[0, 2] += optimizer.gBest_score
+    table[1, 2] += end - start
+    table[2, 2] = worst
+    table[3, 2] = best    
+    
+    x_max = 10*np.ones(d)
+    x_min = -10*np.ones(d)
+    optimizer = WOA(fit_func=Rosenbrock,
+                    num_dim=d, num_particle=p, max_iter=g, x_max=x_max, x_min=x_min)
+    start = time.time()
+    optimizer.opt()
+    end = time.time()
+    if optimizer.gBest_score>worst: worst = optimizer.gBest_score
+    if optimizer.gBest_score<best: best = optimizer.gBest_score    
+    table[0, 3] += optimizer.gBest_score
+    table[1, 3] += end - start
+    table[2, 3] = worst
+    table[3, 3] = best    
+    
+    x_max = 100*np.ones(d)
+    x_min = -100*np.ones(d)
+    optimizer = WOA(fit_func=Step,
+                    num_dim=d, num_particle=p, max_iter=g, x_max=x_max, x_min=x_min)
+    start = time.time()
+    optimizer.opt()
+    end = time.time()
+    if optimizer.gBest_score>worst: worst = optimizer.gBest_score
+    if optimizer.gBest_score<best: best = optimizer.gBest_score    
+    table[0, 4] += optimizer.gBest_score
+    table[1, 4] += end - start
+    table[2, 4] = worst
+    table[3, 4] = best    
+    
+    x_max = 1.28*np.ones(d)
+    x_min = -1.28*np.ones(d)
+    optimizer = WOA(fit_func=Quadric_Noise,
+                    num_dim=d, num_particle=p, max_iter=g, x_max=x_max, x_min=x_min)
+    start = time.time()
+    optimizer.opt()
+    end = time.time()
+    if optimizer.gBest_score>worst: worst = optimizer.gBest_score
+    if optimizer.gBest_score<best: best = optimizer.gBest_score    
+    table[0, 5] += optimizer.gBest_score
+    table[1, 5] += end - start
+    table[2, 5] = worst
+    table[3, 5] = best    
+    
+    x_max = 500*np.ones(d)
+    x_min = -500*np.ones(d)
+    optimizer = WOA(fit_func=Schwefel,
+                    num_dim=d, num_particle=p, max_iter=g, x_max=x_max, x_min=x_min)
+    start = time.time()
+    optimizer.opt()
+    end = time.time()
+    if optimizer.gBest_score>worst: worst = optimizer.gBest_score
+    if optimizer.gBest_score<best: best = optimizer.gBest_score    
+    table[0, 6] += optimizer.gBest_score
+    table[1, 6] += end - start
+    table[2, 6] = worst
+    table[3, 6] = best    
+    
+    x_max = 5.12*np.ones(d)
+    x_min = -5.12*np.ones(d)
+    optimizer = WOA(fit_func=Rastrigin,
+                    num_dim=d, num_particle=p, max_iter=g, x_max=x_max, x_min=x_min)
+    start = time.time()
+    optimizer.opt()
+    end = time.time()
+    if optimizer.gBest_score>worst: worst = optimizer.gBest_score
+    if optimizer.gBest_score<best: best = optimizer.gBest_score    
+    table[0, 7] += optimizer.gBest_score
+    table[1, 7] += end - start
+    table[2, 7] = worst
+    table[3, 7] = best    
+    
+    x_max = 5.12*np.ones(d)
+    x_min = -5.12*np.ones(d)
+    optimizer = WOA(fit_func=Noncontinuous_Rastrigin,
+                    num_dim=d, num_particle=p, max_iter=g, x_max=x_max, x_min=x_min)
+    start = time.time()
+    optimizer.opt()
+    end = time.time()
+    if optimizer.gBest_score>worst: worst = optimizer.gBest_score
+    if optimizer.gBest_score<best: best = optimizer.gBest_score    
+    table[0, 8] += optimizer.gBest_score
+    table[1, 8] += end - start
+    table[2, 8] = worst
+    table[3, 8] = best    
+    
+    x_max = 32*np.ones(d)
+    x_min = -32*np.ones(d)
+    optimizer = WOA(fit_func=Ackley, 
+                    num_dim=d, num_particle=p, max_iter=g, x_max=x_max, x_min=x_min)
+    start = time.time()
+    optimizer.opt()
+    end = time.time()
+    if optimizer.gBest_score>worst: worst = optimizer.gBest_score
+    if optimizer.gBest_score<best: best = optimizer.gBest_score    
+    table[0, 9] += optimizer.gBest_score
+    table[1, 9] += end - start
+    table[2, 9] = worst
+    table[3, 9] = best    
+    
+    x_max = 600*np.ones(d)
+    x_min = -600*np.ones(d)
+    optimizer = WOA(fit_func=Griewank,
+                    num_dim=d, num_particle=p, max_iter=g, x_max=x_max, x_min=x_min)
+    start = time.time()
+    optimizer.opt()
+    end = time.time()
+    if optimizer.gBest_score>worst: worst = optimizer.gBest_score
+    if optimizer.gBest_score<best: best = optimizer.gBest_score    
+    table[0, 10] += optimizer.gBest_score
+    table[1, 10] += end - start
+    table[2, 10] = worst
+    table[3, 10] = best    
+    
+table[:2, :] = table[:2, :] / times
+table = pd.DataFrame(table)
+table.columns=['Sphere', 'Schwefel_P222', 'Quadric', 'Rosenbrock', 'Step', 'Quadric_Noise', 'Schwefel', 
+                'Rastrigin', 'Noncontinuous_Rastrigin', 'Ackley', 'Griewank']
+table.index = ['mean score', 'mean time', 'worst', 'best']
